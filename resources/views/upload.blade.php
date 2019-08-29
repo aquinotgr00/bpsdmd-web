7791cde91dd140f464f405ec95e5c9da55ad324b
Dody Adi N <doddyadin@gmail.com>
2019-08-29 17:23:45 +0700

    add feeder uplaoad

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,13 +1,12 @@
 @extends('layout.main')
-@section('style')
-<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
-@endsection
 
 @section('content')
+
 @php
 $loggedUser = get_user_data();
 $year = date('Y');
 @endphp
+
 <section class="content-header">
 	<h1>Upload File</h1>
 </section>
@@ -34,29 +33,6 @@ $year = date('Y');
 					@endif
 					<form method="post" enctype="multipart/form-data">
 						@csrf
-						<div class="form-group">
-							<label for="usr">Tanggal :</label>
-							<input type="text" class="form-control datepicker" id="usr" name="created_at">
-						</div>
-						<div class="input-group">
-							<div class="input-group-prepend">
-								<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-								<span class="help-block">Ketentuan nama file : ddmmyyyy_nama_file.xls</span>
-								<span class="help-block">Ketentuan maksimal Ukuran file : 1Mb</span>
-
-							</div>
-							<div class="custom-file">
-								<input type="file" class="custom-file-input" id="inputGroupFile01"
-								aria-describedby="inputGroupFileAddon01" name="file">
-								<span class="help-block ">{!! implode('', $errors->get('file')) !!}</span>
-
-							</div>
-						</div>
-						<div class="box-footer">
-							<button class="btn btn-primary pull-right">Simpan</button>
-						</div>
-					</form>
-					<form method="post" enctype="multipart/form-data">
 
 						<div class="page-header">
 							<h2>{{$loggedUser->getOrg()->getName()}}.</h2>
@@ -90,20 +66,24 @@ $year = date('Y');
 									@foreach($files as $key => $file)
 									<tr>
 										<td>{{ $key+1 }}</td>
-										<td>Suplai data bulan {{ date('F',mktime(0, 0, 0, ($key+1), 10)) }}</td>
+										<td>
+											<input type="hidden" name="months[]" value="{{ sprintf('%02s', $key+1) }}">
+											Suplai data bulan {{ date('F',mktime(0, 0, 0, ($key+1), 10)) }}
+										</td>
 										<td style="text-align: center;width: 30%">
+											<input type="hidden" name="file_id[]" value="{{ !empty($file->id) ? $file->id : '' }}">
 											{!! !empty($file->file_name) ? $file->file_name :'<span class="text-red">- File belum tersedia -</span>' !!}
 										</td>
 										<td style="text-align: center;width: 40%">
 											@if(!empty($file))
-											<label class="btn btn-primary" for="my-file-selector{{ $key+1 }}">
+											<label class="btn btn-success" for="my-file-selector{{ $key+1 }}">
 												<input id="my-file-selector{{ $key+1 }}" type="file" style="display:none" name="files[]" 
 												onchange="$(this).parents('label').next('.file-name').html(this.files[0].name)" accept=".xls,.xlsx">
 												Update File
 											</label>
 											<span class='file-name' ></span>
 											@else
-											<label class="btn btn-success" for="my-file-selector{{ $key+1 }}">
+											<label class="btn btn-info" for="my-file-selector{{ $key+1 }}">
 												<input id="my-file-selector{{ $key+1 }}" type="file" style="display:none" name="files[]"
 												onchange="$(this).parents('label').next('.file-name').html(this.files[0].name)" accept=".xls,.xlsx">
 												Upload File
@@ -118,6 +98,9 @@ $year = date('Y');
 								
 							</tbody>
 						</table>
+						<div class="box-footer">
+							<button class="btn btn-primary pull-right">Simpan</button>
+						</div>
 					</form>
 					<div class="box-body"></div>
 				</div>
@@ -128,8 +111,6 @@ $year = date('Y');
 	@endsection
 
 	@section('script')
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js" type="text/javascript"></script>
 	<script type="text/javascript" name>
 		$(function() {
 			$(document).on('change', 'select[name=year]', function(event) {
