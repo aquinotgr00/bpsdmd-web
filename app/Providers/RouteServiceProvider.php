55c34cc124009f2483464fa2d122856760a92b4f
Gabriella <gabriella.vindy@gmail.com>
2019-09-27 19:09:52 +0700

    progress

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         Route::bind('user', $bindToEntityOr404(Entities\User::class));
         Route::bind('org', $bindToEntityOr404(Entities\Organization::class));
+        Route::bind('program', $bindToEntityOr404(Entities\StudyProgram::class));
         parent::boot();
     }
 
---
+++ resources/views/program/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layout.main')
+
+@section('content')
+    <section class="content-header">
+        <h1>Data Instansi</h1>
+        <ol class="breadcrumb">
+            <li>
+                <a href="{{ url(route('prodi.create')) }}">
+                    <i class="fa fa-plus-circle"></i> Tambah instansi
+                </a>
+            </li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-body">
+                        @include('layout.partial.alert')
+
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Nama</th>
+                                    <th>Tipe</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $no = 1 + ($page > 1 ? ($page - 1) * 10 : 0);
+                                /** @var \App\Entities\Organization $item */
+                                foreach ($data as $item) {
+                                ?>
+                                <tr class="even pointer">
+                                    <td>{{ $no++ }}</td>
+                                    <td>{{ $item->getName() }}</td>
+                                    <td>{{ ucfirst($item->getType()) }}</td>
+                                    <td>
+                                        <a href="{{ url(route('prodi.detail', [$item->getId()])) }}"><i class="fa fa-user"></i> Program Studi</a>
+                                    </td>
+                                </tr>
+                                <?php
+                                }
+                                ?>
+
+                                @if(!count($data))
+                                    <tr class="even pointer">
+                                        <td colspan="4">Tidak ada data.</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                        <nav>
+                            {{ $data->links() }}
+                        </nav>
+                    </div>
+                </div><!-- /.box -->
+            </div>
+        </div>
+    </section><!-- /.content -->
+@endsection
