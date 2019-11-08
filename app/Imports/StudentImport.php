51c767043a75f694c058143e6c51c6da44748985
Gabriella <gabriella.vindy@gmail.com>
2019-11-08 16:14:01 +0700

    fix feeder siswa

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -10,10 +10,11 @@ use App\Services\Application\AuthService;
 use App\Services\Domain\ProgramService;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class StudentImport implements ToCollection
 {
-    /** @var sOrganization $org */
+    /** @var Organization $org */
     private $org;
 
     public function setOrg(Organization $org)
@@ -30,31 +31,66 @@ class StudentImport implements ToCollection
     {
         foreach ($cols as $key => $col) 
         {
-            if($key == 0){
+            if($key == 0 || is_null($col[5])){
                 continue;
             }
 
             $studyProgram = null;
-            if(!is_null($col[7])){
+            if(!is_null($col[3])){
                 $programService = new ProgramService;
-                $studyProgram   = $programService->getRepository()->findOneBy(['name' => $col[7], 'org' => $this->org]);
+                $studyProgram   = $programService->getRepository()->findOneBy(['name' => $col[3], 'org' => $this->org]);
                 if ($studyProgram == null) {
-                  $data = collect(['code' => substr($col[4], strpos($col[4], "-") + 1), 'name' => $col[7]]);
+                  $data = collect(['code' => substr($col[0], strpos($col[0], "-") + 1), 'name' => $col[3]]);
                   $programService->create($data, [], $this->org);
-                  $studyProgram = $programService->getRepository()->findOneBy(['name' => $col[7]]);
+                  $studyProgram = $programService->getRepository()->findOneBy(['name' => $col[3]]);
                 }
             }
 
             $student = new Student;
             $student->setCode(substr($col[0], strpos($col[0], "-") + 1));
-            $student->setIdentityNumber($col[1]);
-            $student->setName($col[2]);
-            $student->setPeriod($col[9]);
-            $student->setCurriculum($col[11]);
-            $student->setStatus($col[13]);
-            $student->setClass($col[14]);
-            $student->setIpk($col[16]);
-            $student->setGraduationYear($col[17]);
+            $student->setIdDikti($col[1]);
+            $student->setNim($col[4]);
+            $student->setName($col[5]);
+            $student->setGender($col[6]);
+            $student->setPlaceOfBirth($col[7]);
+            $student->setAddress($col[9]);
+            $student->setPhoneNumber($col[10]);
+            $student->setMobilePhoneNumber($col[11]);
+            $student->setEmail($col[12]);
+            $student->setReligion($col[13]);
+            $student->setMotherName($col[14]);
+            $student->setNationality($col[15]);
+            $student->setForeignCitizen($col[16]);
+            $student->setSocialProtectionCard($col[17]);
+            $student->setOccupationType($col[18]);
+            $student->setIdentityNumber($col[19]);
+            $student->setStartSemester((int)$col[22]);
+            $student->setCurrentSemester((int)$col[23]);
+            $student->setStudentCredits((int)$col[24]);
+            $student->setIpk($col[25]);
+            $student->setCertificateNumber($col[26]);
+            $student->setEnrollmentType($col[28]);
+            $student->setGraduationType($col[29]);
+            $student->setPeriod($col[31]);
+            $student->setCurriculum($col[32]);
+            $student->setStatus($col[33]);
+            $student->setGraduationYear($col[34]);
+
+            if ($col[8]) {
+                $student->setDateOfBirth(date_format(Date::excelToDateTimeObject($col[8]), 'd-m-Y'));
+            }
+            if ($col[20]) {
+                $student->setEnrollmentDateStart(date_format(Date::excelToDateTimeObject($col[20]), 'd-m-Y'));
+            }
+            if ($col[21]) {
+                $student->setEnrollmentDateEnd(date_format(Date::excelToDateTimeObject($col[21]), 'd-m-Y'));
+            }
+            if ($col[27]) {
+                $student->setGraduationJudgementDate(date_format(Date::excelToDateTimeObject($col[27]), 'd-m-Y'));
+            }
+            if ($col[30]) {
+                $student->setLastUpdate(date_format(Date::excelToDateTimeObject($col[30]), 'd-m-Y'));
+            }
 
             if ($this->org instanceof Organization) {
                 $student->setOrg($this->org);
