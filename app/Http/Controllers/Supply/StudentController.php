25ab00b04036af017ed74d36e7ab86756071d7b5
wandy purnomo <wandypurnomo92@gmail.com>
2019-12-08 22:23:26 +0700

    Menambahkan resource programstudi ke Supply/StudentController.php di controller

--- app/Http/Controllers/Supply/StudentController.php
+++ app/Http/Controllers/Supply/StudentController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers\Supply;
 
-use App\Entities\Student;
 use App\Entities\Organization;
+use App\Entities\Student;
 use App\Entities\StudyProgram;
-use App\Imports\StudentImport;
 use App\Http\Controllers\Controller;
-use App\Services\Domain\StudentService;
+use App\Imports\StudentImport;
+use App\Services\Application\AuthService;
+use App\Services\Domain\FeederService;
 use App\Services\Domain\OrgService;
 use App\Services\Domain\ProgramService;
-use App\Services\Domain\FeederService;
-use App\Services\Application\AuthService;
+use App\Services\Domain\StudentService;
+use DateTime;
+use DB;
 use Exception;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
 use Image;
+use Maatwebsite\Excel\Facades\Excel;
 
 class StudentController extends Controller
 {
@@ -26,17 +28,19 @@ class StudentController extends Controller
 
         //build urls
         $urlCreate   = url(route('supply.student.create'));
-        $urlUpdate   = function($id) {
+        $urlUpdate = function ($id) {
             return url(route('supply.student.update', [$id]));
         };
-        $urlDelete   = function($id) {
+        $urlDelete = function ($id) {
             return url(route('supply.student.delete', [$id]));
         };
-        $urlDetail   = '/student';
+        $urlDetail = '/student';
         $urlTemplate = url(route('supply.student.template.download'));
-        $urlUpload   = url(route('supply.student.upload'));
+        $urlUpload = url(route('supply.student.upload'));
 
-        return view('student.index', compact('data', 'page', 'urlCreate', 'urlUpdate', 'urlDelete', 'urlDetail', 'urlTemplate', 'urlUpload'));
+        $studyPrograms = DB::table("program_studi")->where("instansi_id", currentUser()->getOrg()->getId())->select("id", "nama")->get()->toArray();
+
+        return view('student.index', compact('data', 'page', 'urlCreate', 'urlUpdate', 'urlDelete', 'urlDetail', 'urlTemplate', 'urlUpload', 'studyPrograms'));
     }
 
     public function create(Request $request, StudentService $studentService, OrgService $orgService, ProgramService $programService)
@@ -236,7 +240,7 @@ class StudentController extends Controller
                 'identity_number' => $data->getIdentityNumber() ? $data->getIdentityNumber() : '-',
                 'gender' => $data->getGender() ? ucfirst($data->getGender()) : '-',
                 'place_of_birth' => $data->getPlaceOfBirth() ? $data->getPlaceOfBirth() : '-',
-                'date_of_birth' => $data->getDateOfBirth() instanceof \DateTime ? $data->getDateOfBirth()->format('d F Y') : '-',
+                'date_of_birth' => $data->getDateOfBirth() instanceof DateTime ? $data->getDateOfBirth()->format('d F Y') : '-',
                 'address' => $data->getAddress() ? $data->getAddress() : '-',
                 'phone_number' => $data->getPhoneNumber() ? $data->getPhoneNumber() : '-',
                 'mobile_phone_number' => $data->getMobilePhoneNumber() ? $data->getMobilePhoneNumber() : '-',
@@ -247,20 +251,20 @@ class StudentController extends Controller
                 'foreign_citizen' => $data->getForeignCitizen() == 't' ? trans('common.yes') : trans('common.no'),
                 'social_protection_card' => $data->getSocialProtectionCard() == 't' ? trans('common.yes') : trans('common.no'),
                 'occupation_type' => $data->getOccupationType() ? $data->getOccupationType() : '-',
-                'enrollment_date_start' => $data->getEnrollmentDateStart() instanceof \DateTime ? $data->getEnrollmentDateStart()->format('d F Y') : '-',
-                'enrollment_date_end' => $data->getEnrollmentDateEnd() instanceof \DateTime ? $data->getEnrollmentDateEnd()->format('d F Y') : '-',
+                'enrollment_date_start' => $data->getEnrollmentDateStart() instanceof DateTime ? $data->getEnrollmentDateStart()->format('d F Y') : '-',
+                'enrollment_date_end' => $data->getEnrollmentDateEnd() instanceof DateTime ? $data->getEnrollmentDateEnd()->format('d F Y') : '-',
                 'start_semester' => $data->getStartSemester() ? $data->getStartSemester() : '-',
                 'current_semester' => $data->getCurrentSemester() ? $data->getCurrentSemester() : '-',
                 'student_credits' => $data->getStudentCredits() ? $data->getStudentCredits() : '-',
                 'certificate_number' => $data->getCertificateNumber() ? $data->getCertificateNumber() : '-',
-                'graduation_judgement_date' => $data->getGraduationJudgementDate() instanceof \DateTime ? $data->getGraduationJudgementDate()->format('d F Y') : '-',
+                'graduation_judgement_date' => $data->getGraduationJudgementDate() instanceof DateTime ? $data->getGraduationJudgementDate()->format('d F Y') : '-',
                 'enrollment_type' => $data->getEnrollmentType() ? $data->getEnrollmentType() : '-',
                 'graduation_type' => $data->getGraduationType() ? $data->getGraduationType() : '-',
                 'status' => $data->getStatus() ? $data->getStatus() : '-',
                 'class' => $data->getClass() ? $data->getClass() : '-',
                 'ipk' => $data->getIpk() ? $data->getIpk() : '-',
                 'graduation_year' => $data->getGraduationYear() ? $data->getGraduationYear() : '-',
-                'photo' => $data->getPhoto() ? url(url(Student::UPLOAD_PATH.'/'.$data->getPhoto())) : url('img/avatar.png'),
+                'photo' => $data->getPhoto() ? url(url(Student::UPLOAD_PATH . '/' . $data->getPhoto())) : url('img/avatar.png'),
             ];
 
             return response()->json($data);
